2800ba9bfeb190ba96e148db05a211545a400169
Jayeshbagul <jbagul366@gmail.com>
2025-02-26 18:21:48 +0530

    done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -49,7 +49,6 @@ const Home = () => {
                     </div>
                 </div>
             </section >
-
             <section className='view-3'>
                 <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff0189107c3469a831cb0a_home-about-p-800.webp" alt="" />
                 <div className="txt">
@@ -62,7 +61,76 @@ const Home = () => {
                     <p className='vt2'>What we do</p>
                 </div>
             </section>
+            <section className='view-4'>
+                <div className="services-main">
+                    <h1>OUR SERVICES</h1>
+                    <div id="line2"></div>
 
+                    <div className="services">
+                        <div className="service-box">
+                            <div id="line"></div>
+                            <h1>BRANDING</h1>
+                            <p>Establish a strong brand identity that sets you apart.</p>
+                            <div className="btn">
+                                <button>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                                    </svg></button>
+                                <p>ABOUT BRANDING</p>
+                            </div>
+                        </div>
+                        {/* <div id="line"></div> */}
+                        <div className="service-box">
+                            <div id="line"></div>
+                            <h1>Campaigns</h1>
+                            <p>Utilize data and analytics to optimize your marketing performance.</p>
+                            <div className="btn">
+                                <button>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                                    </svg></button>
+                                <p>ABOUT CAMPAIGNS</p>
+                            </div>
+                        </div>
+                        {/* <div id="line"></div> */}
+                        <div className="service-box">
+                            <div id="line"></div>
+                            <h1>E-Commerce</h1>
+                            <p>Develop customized online stores that reflect your brand and vision.</p>
+                            <div className="btn">
+                                <button>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                                    </svg></button>
+                                <p>ABOUT E-COMMERCE</p>
+                            </div>
+                        </div>
+
+                        <div className="service-box">
+                            <div id="line"></div>
+                            <h1>Social Media</h1>
+                            <p>Create engaging and shareable content that drives brand awareness.</p>
+                            <div className="btn">
+                                <button>
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                                    </svg></button>
+                                <p>ABOUT SOCIAL MEDIA</p>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                <div className="pic">
+                    <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff013040f8ceac6b7f98d1_home-services-p-1080.webp" alt="" />
+                </div>
+            </section>
+            <section className='view-5'>
+                <img src="https://cdn.prod.website-files.com/63fd93c920392238faa118c4/63fedc0657c2b7a94573a859_heading-selected-work.svg" alt="" />
+
+            </section>
         </>
     )
 }
