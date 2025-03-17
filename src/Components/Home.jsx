8670f96868766aac15b4cfdd65e83022cba6a8c9
Jayeshbagul <jbagul366@gmail.com>
2025-03-17 15:52:47 +0530

    done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -127,10 +127,162 @@ const Home = () => {
                     <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff013040f8ceac6b7f98d1_home-services-p-1080.webp" alt="" />
                 </div>
             </section>
-            <section className='view-5'>
+            {/* <section className='view-5'>
                 <img src="https://cdn.prod.website-files.com/63fd93c920392238faa118c4/63fedc0657c2b7a94573a859_heading-selected-work.svg" alt="" />
 
+            </section> */}
+
+            {/* clinets section view-8 */}
+            <div className="view-8">
+                <h1>CLIENTS</h1>
+
+                <div className="clients-main">
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2023</h5>
+                            <h1>OCENIA</h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2023</h5>
+                            <h1>Fleuré</h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2022</h5>
+                            <h1>Northland
+                            </h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2022</h5>
+                            <h1>Véloce
+                            </h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2021</h5>
+                            <h1>Azure
+                            </h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                    <div className="box">
+                        <div className="sub-box">
+                            <h5>2021</h5>
+                            <h1>Sage
+                            </h1>
+                        </div>
+                        <span>BRANDING</span>
+                    </div>
+                    <div id="line"></div>
+                </div>
+
+            </div>
+            <section className='view-6'>
+                <div className="main-team">
+                    <div className="bio">
+                        <h1>TEAM</h1>
+                        <p>We have assembled a team of talented and experienced professionals who are dedicated to helping you achieve your brand's full potential.</p>
+                    </div>
+                    <div className="team-main">
+                        <div className="card">
+                            <h1>EMILY JOY</h1>
+                            <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff00a2db0240de50310796_team-01-p-500.webp" alt="" />
+                            <div className="card-info">
+                                <h4>CREATIVE DIRECTOR</h4>
+                                <div className="main-icon">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </div>
+                            </div>
+                            <p>Leading our creative vision with strategic insights and innovative ideas.</p>
+                        </div>
+                        <div className="card">
+                            <h1>Kate Lee</h1>
+                            <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff00a271f3fd9dfe241c66_team-02-p-500.webp" alt="" />
+                            <div className="card-info">
+                                <h4>Brand Designer</h4>
+                                <div className="main-icon">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </div>
+                            </div>
+                            <p>Crafting unique brand identities that capture the essence of your business.</p>
+                        </div>
+                        <div className="card">
+                            <h1>Kate Lee</h1>
+                            <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff00a271f3fd9dfe241c66_team-02-p-500.webp" alt="" />
+                            <div className="card-info">
+                                <h4>Brand Designer</h4>
+                                <div className="main-icon">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </div>
+                            </div>
+                            <p>Crafting unique brand identities that capture the essence of your business.</p>
+                        </div>
+                        <div className="card">
+                            <h1>Katy Yen</h1>
+                            <img src="https://assets-global.website-files.com/63fd93c920392238faa118c4/63ff00a343ac9c5c6dfc2f0c_team-04-p-500.webp" alt="" />
+                            <div className="card-info">
+                                <h4>Fashion Designer</h4>
+                                <div className="main-icon">
+                                    <i class="fa-brands fa-instagram"></i>
+                                    <i class="fa-brands fa-facebook"></i>
+                                    <i class="fa-brands fa-x-twitter"></i>
+                                </div>
+                            </div>
+                            <p>Bringing your brand to life through thoughtful design and attention to detail.</p>
+                        </div>
+                    </div>
+                </div>
             </section>
+
+            <section className='view-7'>
+                <div className="testimonials">
+                    <div id="line"></div>
+                    <div className="join-main">
+                        <h5>JOIN THE TEAM</h5>
+                        <h2>WANT THE SHAPE OF FUTURE OF BRANDING ?</h2>
+                        <div className="btn">
+                            <button>
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                                </svg></button>
+                            <p>FIND JOB</p>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section className='view-last'>
+                <div className="main">
+                    <img src="https://cdn.prod.website-files.com/63fd93c920392238faa118c4/63feeb42b65dd173f3735367_heading-lets-create-inverse.svg" alt="" />
+                    <h1>Unleashing the Potential of Your Brand</h1>
+                    <button>Get In Touch  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M13.922 4.5V11.8125C13.922 11.9244 13.8776 12.0317 13.7985 12.1108C13.7193 12.1899 13.612 12.2344 13.5002 12.2344C13.3883 12.2344 13.281 12.1899 13.2018 12.1108C13.1227 12.0317 13.0783 11.9244 13.0783 11.8125V5.51953L4.79547 13.7953C4.71715 13.8736 4.61092 13.9176 4.50015 13.9176C4.38939 13.9176 4.28316 13.8736 4.20484 13.7953C4.12652 13.717 4.08252 13.6108 4.08252 13.5C4.08252 13.3892 4.12652 13.283 4.20484 13.2047L12.4806 4.92188H6.18765C6.07577 4.92188 5.96846 4.87743 5.88934 4.79831C5.81023 4.71919 5.76578 4.61189 5.76578 4.5C5.76578 4.38811 5.81023 4.28081 5.88934 4.20169C5.96846 4.12257 6.07577 4.07813 6.18765 4.07812H13.5002C13.612 4.07813 13.7193 4.12257 13.7985 4.20169C13.8776 4.28081 13.922 4.38811 13.922 4.5Z" fill="currentColor"></path>
+                    </svg></button>
+                </div>
+            </section>
+
         </>
     )
 }
